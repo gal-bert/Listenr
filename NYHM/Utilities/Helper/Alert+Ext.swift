cc06541771fbf9fc1f5fa66dca02ed5202f1e259
Karen Natalia <karen.natalia888@gmail.com>
2022-06-22 16:27:09 +0700

    add max character validation in add tag alert

--- NYHM/Utilities/Helper/Alert+Ext.swift
+++ NYHM/Utilities/Helper/Alert+Ext.swift
@@ -24,16 +24,34 @@ extension UIViewController {
         
         alert.overrideUserInterfaceStyle = .dark
         alert.view.tintColor = UIColor(red: 0.99, green: 0.82, blue: 0.15, alpha: 1.00)
+        
         alert.addTextField { textField in
             textField.placeholder = "Enter tag name"
-            NotificationCenter.default.addObserver(forName: UITextField.textDidChangeNotification, object: textField, queue: OperationQueue.main, using:
-                                                    {_ in
+            NotificationCenter.default.addObserver(forName: UITextField.textDidChangeNotification, object: textField, queue: OperationQueue.main, using: {_ in
                 let textCount = textField.text?.trimmingCharacters(in: .whitespacesAndNewlines).count ?? 0
                 let textIsNotEmpty = textCount > 0
-                
                 addAction.isEnabled = textIsNotEmpty
+                alert.textFields![1].text = "\(textCount) of 20 characters"
+                
+                if textCount > 20 {
+                    textField.text?.removeLast()
+                    alert.textFields![1].text = "20 of 20 characters"
+                }
             })
         }
+        alert.addTextField { textField in
+            textField.text = "0 of 20 characters"
+            textField.textAlignment = .center
+            textField.isUserInteractionEnabled = false
+        }
+        
+        if let textFields = alert.textFields {
+            if textFields.count > 0 {
+                textFields[1].superview!.superview!.subviews[0].removeFromSuperview()
+                textFields[1].superview!.backgroundColor = UIColor.clear
+            }
+        }
+        
         alert.addAction(cancelAction)
         alert.addAction(addAction)
         
