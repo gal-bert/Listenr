a08e97906e030b6a726f880c472f309b1cb6088e
Gregorius Albert <galbert.djaja@gmail.com>
2023-01-12 03:06:19 +0700

    hotfix timer for waveform

--- NYHM/Feature/Transcription/Sinwaveform/WaveController.swift
+++ NYHM/Feature/Transcription/Sinwaveform/WaveController.swift
@@ -12,7 +12,7 @@ import UIKit
 extension TranscriptionViewController {
     
     func startWave() {
-        waveformTimer = Timer.scheduledTimer(withTimeInterval: 0.02, repeats: true, block: { [self] timer in
+        waveformTimer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true, block: { [self] timer in
             self.audioRecorder?.updateMeters()
             self.writeWaves((self.audioRecorder?.averagePower(forChannel: 0)) ?? 0, true, waveCounterTimer)
             
@@ -22,7 +22,7 @@ extension TranscriptionViewController {
     
     func writeWaves(_ input: Float, _ bool: Bool, _ count: CGFloat) {
         if !bool {
-            start = firstPoint
+            self.start = firstPoint
             if waveformTimer != nil || audioRecorder != nil {
                 waveformTimer.invalidate()
             }
@@ -30,37 +30,40 @@ extension TranscriptionViewController {
         }
         else {
             if input < -55 {
-                amplitudeLength = 0.2
+                self.amplitudeLength = 0.2
             }
             else if input < -40 && input > -55 {
-                amplitudeLength = (CGFloat(input) + 56) / 3
+                self.amplitudeLength = (CGFloat(input) + 56) / 3
             }
             else if input < -20 && input > -40 {
-                amplitudeLength = (CGFloat(input) + 41) / 2
+                self.amplitudeLength = (CGFloat(input) + 41) / 2
             }
             else if input < -10 && input > -20 {
-                amplitudeLength = (CGFloat(input) + 21) * 5
+                self.amplitudeLength = (CGFloat(input) + 21) * 5
             }
             else {
-                amplitudeLength = (CGFloat(input) + 20) * 4
+                self.amplitudeLength = (CGFloat(input) + 20) * 4
             }
             
-            pencil.move(to: start)
-            pencil.addLine(to: CGPoint(x: start.x, y: start.y + amplitudeLength))
+            DispatchQueue.main.async {
+                self.pencil.move(to: self.start)
+                self.pencil.addLine(to: CGPoint(x: self.start.x , y: self.start.y + self.amplitudeLength))
+                
+                self.pencil.move(to: self.start)
+                self.pencil.addLine(to: CGPoint(x: self.start.x, y: self.start.y - self.amplitudeLength))
+                
+                self.waveLayer.path = self.pencil.cgPath
+                self.waveLayer.strokeColor = UIColor(named: "blueText")?.cgColor
+                self.waveLayer.fillColor = UIColor.clear.cgColor
+                self.waveLayer.lineWidth = 1
+                self.waveLayer.position = CGPoint(x: 0 - (self.space * count), y: 0)
+                
+                self.waveformView.layer.addSublayer(self.waveLayer)
+                self.waveformView.setNeedsDisplay()
+                
+                self.start = CGPoint(x: self.start.x + self.space, y: self.start.y)
+            }
             
-            pencil.move(to: start)
-            pencil.addLine(to: CGPoint(x: start.x, y: start.y - amplitudeLength))
-            
-            waveLayer.path = pencil.cgPath
-            waveLayer.strokeColor = UIColor(named: "blueText")?.cgColor
-            waveLayer.fillColor = UIColor.clear.cgColor
-            waveLayer.lineWidth = 1
-            waveLayer.position = CGPoint(x: 0 - (space * count), y: 0)
-
-            waveformView.layer.addSublayer(waveLayer)
-            waveformView.setNeedsDisplay()
-            
-            start = CGPoint(x: start.x + space, y: start.y)
             
         }
     }
